9ae8c68f0d68937a18610be0ec1e0f4301560e17
Kumar Aditya Raj <155299527+adityaraj1017@users.noreply.github.com>
2025-08-26 18:34:50 +0000

    Variable in javascript

---
+++ 01_Basic/01_Variable.js
@@ -0,0 +1,17 @@
+// Different way of declaring variable
+const userId = 10170034
+var userCity = "Patna"
+let userName = "Piyakkad"
+userDish = "Liti chokha"
+var userAddress;
+
+/*
+    This are the different way of declaring the variable.
+    const keyword denote that its value cann't be change.
+    var and let value is manipulate.
+    var have some problem with scope so we used let to deal with that problem.
+    We can also declare variable without using keyword but it is bad practise.
+    If we have only declare the varible without assining any value then the value of variable is undefined.
+*/
+
+console.table([userId,userName,userCity,userDish]);
---
+++ 01_Basic/02_Datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js as new varsion
+
+// alert(4 + 9) Only allow by the browser.
+
+// Write the code which is easily readable by different user.
+
+/*
+    Different types of datatypes are as follow:-
+    number => 2 to power of 53
+    bigint
+    boolean => true/false
+    string = ""
+    null => Standalone value
+    undefined
+    symbol => unique
+
+    Object
+*/
+
+console.log(typeof "Piyakkad") 
+// Used to find the data types.
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
